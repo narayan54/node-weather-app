1822741e5fd30853fe9019e7035df7f445f89a64
Narayan54 <narayan@theatrolabs.com>
2020-07-07 16:25:45 +0530

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('../src/utils/geocode');
 const forecast = require('../src/utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // console.log(__dirname);
 // console.log(path.join(__dirname, "../public"));
@@ -116,6 +117,6 @@ app.get('*', (req,res) =>{
     });
 });
 
-app.listen('3000', () => {
-    console.log("Server started");
+app.listen(port, () => {
+    console.log("Server started" + port);
 })
